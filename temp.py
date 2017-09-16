24a2c52b78205633ba940194b78ca179d1bb45bc
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-09-16 11:20:05 -0400

    Deal with Categorical Data

--- temp.py
+++ temp.py
@@ -4,8 +4,10 @@ Spyder Editor
 
 This is a temporary script file.
 """
-clear
+
 print ("Hello Spider Dai Nguyen")
+
+# import libraries
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd #lib to import and manage dataset
@@ -15,10 +17,14 @@ dataset=pd.read_csv('Data.csv')
 X=dataset.iloc[:,:-1].values #select all column except the last column
 Y=dataset.iloc[:,3].values
 
+
+#Taking care of missing data
 from sklearn.preprocessing import Imputer
 
-imputer=Imputer(missing_value='NaN',strategy="mean", axis=0)
+imputer=Imputer(missing_values='NaN',strategy="mean", axis=0)
 
 imputer=imputer.fit(X[:,1:3])
 
-X[:,2]=imputer;
+X[:,1:3]=imputer.transform(X[:,1:3])
+
+#Categorical Data
